68254cdd629aba1a590fc43aa02b7e65b7c38195
SangersJeroen <jeroen98.sangers@gmail.com>
2026-03-03 11:41:41 +0100

    Small build change

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 python -m nuitka \
+    --full-compat \
     --standalone \
+    --enable-plugin=no-qt \
     --nofollow-import-to=ipython,ipywidgets,sympy,pygments \
     --include-distribution-metadata=numpy,scipy,dask,numba,libertem \
     --assume-yes-for-downloads \
